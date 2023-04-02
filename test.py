f4b50008a3b1b68011b5d7818df6fad90cc4ac4a
srchd <sipirichi@gmail.com>
2023-04-02 02:28:29 +0200

    Started working on Windows dev environment Added  file to be able to test if the needed modules can be imported.

---
+++ test.py
@@ -0,0 +1,11 @@
+import cv2
+print('CV2 version: {}'.format(cv2.__version__))
+
+import youtube_api
+print('YT Data API version: {}'.format(youtube_api.__version__))
+
+import numpy as np
+print('NumPy version: {}'.format(np.__version__))
+
+import tkinter
+print('Tkinter version: {}'.format(tkinter.TkVersion))
