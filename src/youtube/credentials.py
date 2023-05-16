f86b2a71c73d48d3978f6804b372bdc0a8ba18b5
GergelyMagyar <m.gergely1999@gmail.com>
2023-05-16 22:12:56 +0200

    fix tests not running 2

--- src/youtube/credentials.py
+++ src/youtube/credentials.py
@@ -17,7 +17,7 @@ from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import argparser, run_flow
 
-from youtube_client_parameters import ApiParameters, YTHDSettings
+from .youtube_client_parameters import ApiParameters, YTHDSettings
 
 
 class DataStorage:
